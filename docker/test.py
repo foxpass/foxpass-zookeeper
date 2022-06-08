f77d24a6b1a90230bb3d805279a6ddda24f99825
Bryan <bojorquebryan@gmail.com>
2022-06-08 17:41:37 +0800

    make sure to check if all the nodes are in synced

--- docker/test.py
+++ docker/test.py
@@ -1,28 +1,32 @@
+import requests
 import os
 
 zks = os.getenv('ZOO_SERVERS')
 zk_stats = {}
 
 for zk in zks.split(' '):
-   zk_host = zk.split('=')[1].split(':')[0]
-   zk_port = zk.split('=')[1].split(':')[2].split(';')[1]
-   zk_stats[zk_host] = os.popen("echo mntr | nc {} {}".format(zk_host, zk_port)).read()
+    zk_host = zk.split('=')[1].split(':')[0]
+    zk_stats[zk_host] = requests.get('http://{}:{}/commands/monitor'.format(zk_host, 8080), timeout=0.01).json()
 
 leader = []
 followers = []
-for zk in zk_stats:
-   stats = zk_stats[zk]
-   for stat in stats.split("\n"):
-      if "zk_server_state" in stat:
-          if "leader" in stat:
-              leader.append(zk)
-          if "follower" in stat:
-              followers.append(zk)
+synced_followers = 0
 
-if len(leader) > 1 or len(leader) < 1 or len(followers) < 2 or len(followers) > 2:
+for zk in zk_stats:
+    stats = zk_stats[zk]
+    if 'server_state' in stats:
+        if stats['server_state'] == 'leader':
+            leader.append(zk)
+            synced_followers = stats['synced_followers']
+        else:
+            followers.append(zk)
+
+if synced_followers < 2 and (len(leader) > 1 or len(leader) < 1 or len(followers) < 2 or len(followers) > 2):
     print("alert!")
     print("stats: {}".format(zk_stats))
+    print("synced_followers: {}".format(synced_followers))
 else:
     print("all OK \n")
     print("leader: {}".format(leader))
     print("followers: {}".format(followers))
+    print("synced_followers: {}".format(synced_followers))
